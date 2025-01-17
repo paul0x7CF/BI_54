3c742bc0cc4247357f12c534cbce44ea2d444787
paul0x7CF <a11919391@unet.univie.ac.at>
2025-01-17 19:59:52 +0100

    Not Null changed

--- BI_54/task1/oltp_db/TB_Person.sql
+++ BI_54/task1/oltp_db/TB_Person.sql
@@ -1,10 +1,10 @@
 CREATE TABLE TB_Person (
     PersonID    INT          NOT NULL,
     FirstName   VARCHAR(50)  NOT NULL,
-    MiddleName  VARCHAR(50)  NOT NULL,
+    MiddleName  VARCHAR(50)  NULL,
     LastName    VARCHAR(50)  NOT NULL,
-    Gender      VARCHAR(1)   NOT NULL,  -- 'M' or 'F'
-    Birthdate   DATE         NOT NULL,
+    Gender      VARCHAR(1),            -- 'M' or 'F'
+    Birthdate   DATE,
     CONSTRAINT PK_Person
         PRIMARY KEY (PersonID)
 );
