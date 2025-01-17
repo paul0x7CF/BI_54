4eb68afa64dfa46994552b9f8732d308fdfe2567
paul0x7CF <a11919391@unet.univie.ac.at>
2025-01-17 17:01:37 +0100

    Not Null to Null changed

--- BI_54/task1/oltp_db/TB_Product.sql
+++ BI_54/task1/oltp_db/TB_Product.sql
@@ -4,10 +4,10 @@ CREATE TABLE TB_Product (
     ProductNumber         VARCHAR(50)     NOT NULL,
     StandardCost          DECIMAL(13,4)   NOT NULL,
     ListPrice             DECIMAL(13,4)   NOT NULL,
-    ProductSubCategoryID  INT             NOT NULL,
-    ProductModelID        INT             NOT NULL,
+    ProductSubCategoryID  INT             NULL,
+    ProductModelID        INT             NULL,
     SellStartDate         DATE            NOT NULL,
-    SellEndDate           DATE            NOT NULL,
+    SellEndDate           DATE			  NULL,
     CONSTRAINT PK_Product
         PRIMARY KEY (ProductID),
     CONSTRAINT FK_ProductSubCategory_Product
