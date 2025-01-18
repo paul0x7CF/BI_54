4e5633f37949d0c32aa5f3ec67e63b6f81308864
paul0x7CF <a11919391@unet.univie.ac.at>
2025-01-18 17:08:42 +0100

    load and transform dim product

---
+++ BI_54/task2/etl_sales/Tr_Dim_Product.sql
@@ -0,0 +1,34 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE BI_BikesDW_54.Dim_Product;
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO BI_BikesDW_54.Dim_Product (
+    ProductKey, 
+    ProductName, 
+    ProductModelName, 
+    ProductSubCategoryName, 
+    ProductTopCategoryName, 
+    StandardCost, 
+    ListPrice, 
+    StartDate, 
+    EndDate, 
+    ProductStatus
+)
+SELECT 
+    P.ProductID AS ProductKey, 
+    P.ProductName, 
+    PM.ProductModelName, 
+    PSC.Name AS ProductSubCategoryName, 
+    PTC.Name AS ProductTopCategoryName, 
+    P.StandardCost, 
+    P.ListPrice, 
+    P.SellStartDate AS StartDate, 
+    P.SellEndDate AS EndDate, 
+    CASE 
+        WHEN P.SellEndDate IS NULL OR P.SellEndDate > '2021-09-30' THEN 'Current'
+        ELSE 'Discontinued'
+    END AS ProductStatus
+FROM BI_Bikes_54.TB_Product P
+JOIN BI_Bikes_54.TB_ProductModel PM ON P.ProductModelID = PM.ProductModelID
+JOIN BI_Bikes_54.TB_ProductSubCategory PSC ON P.ProductSubCategoryID = PSC.ProductSubCategoryID
+JOIN BI_Bikes_54.TB_ProductTopCategory PTC ON PSC.ProductTopCategoryID = PTC.ProductTopCategoryID;
