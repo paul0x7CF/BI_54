ad16e2be3e031084b6012cb6549247827bbdab9e
paul0x7CF <a11919391@unet.univie.ac.at>
2025-01-19 10:45:22 +0100

    save create user in repository

---
+++ CREATE USER bi2024.sql
@@ -0,0 +1,8 @@
+-- <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+-- Step 2. Drop if exists and re-create databases
+-- please make sure you replace <groupNo> with YOUR GROUP NUMBER
+DROP DATABASE IF EXISTS BI_Bikes_54;
+DROP DATABASE IF EXISTS BI_BikesDW_54;
+CREATE DATABASE IF NOT EXISTS BI_Bikes_54;
+CREATE DATABASE IF NOT EXISTS BI_BikesDW_54; 
+-- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
